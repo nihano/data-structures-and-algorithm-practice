deebffb535026aadca15954fb1b776ae875bf3fc
nihano <nioztekin@gmail.com>
2023-02-20 00:18:57 +0300

    KidsWithCandies

---
+++ src/main/java/dsa_AlgoSessions_Fatih/session1/KidsWithCandies.java
@@ -0,0 +1,32 @@
+package dsa_AlgoSessions_Fatih.session1;
+
+import java.util.Arrays;
+
+public class KidsWithCandies {
+    public static void main(String[] args) {
+
+        int[] candies = {2,3,5,1,2};
+        int extraCandies=3;
+
+        System.out.println(Arrays.toString(kidsWithCandies(candies, extraCandies)));
+
+    }
+
+    public static boolean[] kidsWithCandies(int[] candies, int extraCandies) {
+        int max=0;
+        for (int candy : candies) {
+            if (candy>max){
+                max=candy;
+            }
+        }
+
+        boolean[] output = new boolean[candies.length];
+
+        for (int i = 0; i < candies.length; i++) {
+            output[i] = extraCandies+candies[i]>=max;
+
+        }
+        return output;
+
+    }
+}
