0eddaa6faeca3730be702ddc615176efcf6d9ad9
nihano <nioztekin@gmail.com>
2023-04-30 01:53:17 +0300

    LinkedList

---
+++ src/main/java/linked_list/TestLinkedList.java
@@ -0,0 +1,27 @@
+package linked_list;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+        MySinglyLinkedList mySinglyLinkedList = new MySinglyLinkedList();
+        mySinglyLinkedList.add(1);
+        mySinglyLinkedList.add(2);
+        mySinglyLinkedList.add(3);
+
+        mySinglyLinkedList.printLinkedList();
+
+        mySinglyLinkedList.deleteById(3);
+        mySinglyLinkedList.printLinkedList();
+
+        mySinglyLinkedList.deleteById(1);
+        mySinglyLinkedList.printLinkedList();
+
+        mySinglyLinkedList.add(3);
+        mySinglyLinkedList.add(4);
+        mySinglyLinkedList.add(5);
+
+        mySinglyLinkedList.deleteById(3);
+        mySinglyLinkedList.printLinkedList();
+    }
+
+
+}
