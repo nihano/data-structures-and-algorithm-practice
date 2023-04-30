0ec7fff1666feb97b736ed5013a23608d18241b5
nihano <nioztekin@gmail.com>
2023-04-30 15:30:43 +0300

    Node

---
+++ src/main/java/dsa_AlgoSessions_Fatih/Node.java
@@ -0,0 +1,16 @@
+package dsa_AlgoSessions_Fatih;
+
+public class Node {
+    int key;
+    int value;
+    int index;
+    Node next;
+
+    public Node() {
+    }
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
