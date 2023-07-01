4db9e782bf002eee83e0dab115d9c7202fe36c6e
nihano <nioztekin@gmail.com>
2023-07-01 15:28:51 +0300

    LinkedList reverse

---
+++ src/main/java/linkedList/leetcode/TestLinkedList.java
@@ -0,0 +1,19 @@
+package linkedList.leetcode;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+        MySinglyLinkedList linkedList = new MySinglyLinkedList();
+        linkedList.addData(1);
+        linkedList.addData(2);
+        linkedList.addData(3);
+        linkedList.addData(4);
+        linkedList.addData(5);
+        linkedList.addData(6);
+        linkedList.addData(7);
+        linkedList.printNodes();
+        MySinglyLinkedList.reverse(linkedList);
+        linkedList.printNodes();
+        System.out.println(linkedList.head.value);
+        System.out.println(linkedList.tail.value);
+    }
+}
