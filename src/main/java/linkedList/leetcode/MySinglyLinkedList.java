c26463602adb9c2fd8f2ef2a1831ae12b2545e11
nihano <nioztekin@gmail.com>
2023-07-01 21:53:01 +0300

    practice

--- src/main/java/linkedList/leetcode/MySinglyLinkedList.java
+++ src/main/java/linkedList/leetcode/MySinglyLinkedList.java
@@ -31,41 +31,115 @@ public class MySinglyLinkedList {
         }
     }
 
-    void deleteById(int data){
+    void deleteById(int data) {
         Node current = head;
-        Node prev=head;
+        Node prev = head;
         if (isEmpty()) System.out.println("List is empty");
-        while (current!=null){
-            if (current.value==data){
-                if (current==head){
-                    head=current.next;
-                    current.next=null;
-                } else if (current==tail) {
-                   tail=prev;
-                   prev.next=null;
-                }else{
-                    prev.next=current.next;
-                    current.next=null;
+        while (current != null) {
+            if (current.value == data) {
+                if (current == head) {
+                    head = current.next;
+                    current.next = null;
+                } else if (current == tail) {
+                    tail = prev;
+                    prev.next = null;
+                } else {
+                    prev.next = current.next;
+                    current.next = null;
                 }
             }
-            prev=current;
-            current=current.next;
+            prev = current;
+            current = current.next;
         }
     }
 
-    static void reverse(MySinglyLinkedList list){
+    static void reverse(MySinglyLinkedList list) {
         Node prev = null;
-        Node current=list.head;
+        Node current = list.head;
 
-        while (current!=null){
+        while (current != null) {
             Node next = current.next;
-            current.next=prev;
-            prev=current;
-            current=next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
-        list.tail=list.head;
-        list.head=prev;
+        list.tail = list.head;
+        list.head = prev;
     }
 
+    int findKthFromLast(int k) {
+        //0 1 2 3 4 5 6 7
+        /*
+        k=2
+        slow fast
+        0     2
+        1     3
+        2     4
+        3     5
+        4     6
+        5     7
+        6     null
+
+        move fast k times
+        move both one time
+        when fast hit null slow will be on the kth element from last
+        while fast.next.next!=null
+         */
+
+        Node slow = head;
+        Node fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow.value;
+    }
+
+    int getKthItemFromLast(int k) {
+        Node p1 = head;
+        Node p2 = head;
+        for (int i = 0; i < k; i++) {
+            p2 = p2.next;
+        }
+
+        while (p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1.value;
+    }
+
+    void deleteKthItemFromLast(int k) {
+        if (isEmpty()) System.out.println("List is empty");
+        Node slow = head;
+        Node fast = head;
+        Node prev = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        if (slow == head) {
+            head=slow.next;
+            slow.next=null;
+            size--;
+        } else if (slow == tail) {
+            tail = prev;
+            prev.next = null;
+            size--;
+        } else {
+            prev.next = slow.next;
+            slow.next = null;
+            size--;
+        }
+    }
 
 }
