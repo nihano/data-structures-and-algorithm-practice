af506b9ad7f5524a56ae047cc09d5bb362a9c95a
nihano <nioztekin@gmail.com>
2023-07-02 14:04:25 +0300

    practice

---
+++ src/main/java/linkedList/leetcode/PrintMiddleNodes.java
@@ -0,0 +1,26 @@
+package linkedList.leetcode;
+
+public class PrintMiddleNodes {
+    public static void main(String[] args) {
+        MySinglyLinkedList sll = new MySinglyLinkedList();
+        for (int i = 1; i < 7; i++) {
+            sll.addData(i);
+        }
+        printMiddleNodes(sll);
+    }
+
+    static void printMiddleNodes(MySinglyLinkedList sll) {
+        Node slow = sll.head;
+        Node fast = sll.head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast.next == null) {
+            System.out.println(slow.value);
+        } else {
+            System.out.println(slow.value + ", " + slow.next.value);
+        }
+    }
+}
