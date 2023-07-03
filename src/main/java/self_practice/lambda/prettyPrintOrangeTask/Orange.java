f9ddfe09d819bada549f12d8f2459875d8340ece
nihano <nioztekin@gmail.com>
2023-07-03 16:39:16 +0300

    Lambda task

---
+++ src/main/java/self_practice/lambda/prettyPrintOrangeTask/Orange.java
@@ -0,0 +1,16 @@
+package self_practice.lambda.prettyPrintOrangeTask;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import self_practice.lambda.Color;
+
+@Getter
+@AllArgsConstructor
+@Setter
+@ToString
+public class Orange {
+    private int weight;
+    private Color color;
+}
