4a17d895ede01cd0fcebcecb8ac68a03a79f51c0
nihano <nioztekin@gmail.com>
2023-07-03 19:56:54 +0300

    Lambda task

---
+++ src/main/java/self_practice/lambda/SocialNetworkingApp/Gender.java
@@ -0,0 +1,10 @@
+package self_practice.lambda.SocialNetworkingApp;
+
+public enum Gender {
+    MALE("Male"), FEMALE("Female");
+    String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+}
