09513306a0ed3ef38ee960d19efbeef6ee518e20
nihano <nioztekin@gmail.com>
2023-07-03 19:56:57 +0300

    Lambda task

---
+++ src/main/java/self_practice/lambda/SocialNetworkingApp/Person.java
@@ -0,0 +1,17 @@
+package self_practice.lambda.SocialNetworkingApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+    private Gender gender;
+
+
+
+}
