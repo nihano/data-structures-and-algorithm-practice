36e0d361618b42fd4790e5e4bdfeea8048cf4a4f
nihano <nioztekin@gmail.com>
2023-07-03 16:39:26 +0300

    Lambda task

---
+++ src/main/java/self_practice/lambda/prettyPrintOrangeTask/OrangeTest.java
@@ -0,0 +1,32 @@
+package self_practice.lambda.prettyPrintOrangeTask;
+
+import self_practice.lambda.Color;
+import self_practice.lambda.prettyPrintOrangeTask.Orange;
+import self_practice.lambda.prettyPrintOrangeTask.OrangeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrangeTest {
+    public static void main(String[] args) {
+
+        List<Orange> inventory = new ArrayList<Orange>();
+        inventory.add(new Orange(300, Color.GREEN));
+        inventory.add(new Orange(100,Color.RED));
+        inventory.add(new Orange(200,Color.GREEN));
+        inventory.add(new Orange(50,Color.RED));
+
+        OrangeFormatter orangeLambda = orange -> "An orange of " +orange.getWeight()+"g";
+        prettyPrintApple(inventory, orangeLambda);
+
+        prettyPrintApple(inventory, orange -> "An orange of " +orange.getWeight()+"g");
+
+    }
+
+    public static void prettyPrintApple(List<Orange> inventory, OrangeFormatter formatter){
+        for(Orange orange : inventory){
+            String output = formatter.accept(orange);
+            System.out.println(output);
+        }
+    }
+}
