11e9948d99ec126f68b3f2da77f21e4cbbc164ae
nihano <nioztekin@gmail.com>
2023-07-08 23:01:55 +0300

    Stream

---
+++ src/main/java/self_practice/stream/Dish.java
@@ -0,0 +1,13 @@
+package self_practice.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Dish {
+     private String name;
+     private boolean vegetarian;
+     private int calories;
+     private Type type;
+}
