72e24333890e3d4e95af53ec56abf630a6a21bce
nihano <nioztekin@gmail.com>
2023-07-12 00:55:00 +0300

    practice

---
+++ src/main/java/dsa_AlgoSessions_Fatih/session8/PhoneBookTree.java
@@ -0,0 +1,23 @@
+//package dsa_AlgoSessions_Fatih.session8;
+//
+//import dsa_AlgoSessions_Fatih.session8.CustomerNode;
+//
+//import java.util.List;
+//
+//public class PhoneBookTree {
+//
+//    void insert(CustomerNode newNode){
+//
+//    }
+//
+//    void printNamesInAscOrder(CustomerNode root){
+//    }
+//    List<String> firstNamesList(CustomerNode root){ // This is an In Order Traversal Iteratively
+//
+//    }
+//
+//    // return size or number of entries in the phonebook
+//
+//
+//}
+//
