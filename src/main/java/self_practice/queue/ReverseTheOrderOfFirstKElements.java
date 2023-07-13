abd8769048f6c44bdfd8e8d69513898eab2b1619
nihano <nioztekin@gmail.com>
2023-07-13 17:44:50 +0300

    ReverseTheOrderOfFirstKElements

---
+++ src/main/java/self_practice/queue/ReverseTheOrderOfFirstKElements.java
@@ -0,0 +1,32 @@
+package self_practice.queue;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class ReverseTheOrderOfFirstKElements {
+    public static void main(String[] args) {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
+        myQueue.enqueue(4);
+        myQueue.enqueue(5);
+        myQueue.printQueue();
+        reverseTheOrder(myQueue, 3).printQueue();
+    }
+
+    static MyQueue<Integer> reverseTheOrder(MyQueue<Integer> myQueue, int k){
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            stack.push(myQueue.dequeue());
+        }
+        while (!stack.isEmpty()){
+            myQueue.enqueue(stack.pop());
+        }
+        for (int i = 0; i < myQueue.size-k; i++) {
+            Integer item = myQueue.dequeue();
+            myQueue.enqueue(item);
+        }
+        return myQueue;
+    }
+}
