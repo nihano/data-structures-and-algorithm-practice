2bc23ef776e4b52ff634ebf7acebfc9ef36c7e14
nihano <nioztekin@gmail.com>
2023-07-14 18:50:29 +0300

    VisualizeTree

---
+++ src/main/java/self_practice/trees/TreeApp.java
@@ -0,0 +1,12 @@
+package self_practice.trees;
+
+public class TreeApp {
+    public static void main(String[] args) {
+        MyTree tree = new MyTree();
+        int[] numbers = new int[]{10, 5, 12, 3, 6, 11, 13, 2};
+        for (int i = 0; i < 8; i++) {
+            tree.insert(numbers[i]);
+        }
+        VisualizeTree.printTree(tree.root, null, false);
+    }
+}
