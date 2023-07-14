5a57acff2e3a9bf3e33f346a2fea804efcdc1ba1
nihano <nioztekin@gmail.com>
2023-07-14 17:39:36 +0300

    insert

--- src/main/java/self_practice/trees/MyTree.java
+++ src/main/java/self_practice/trees/MyTree.java
@@ -5,4 +5,32 @@ public class MyTree {
 
     public MyTree() {
     }
+
+    void insert(int value){
+        TNode newNode = new TNode(value);
+        if (root==null) {
+            root=newNode;
+            return;
+        }
+        TNode current = root;
+        while (true){
+            if (value<=current.value){
+                if (current.leftChild==null){
+                    //if left is null insert there
+                    current.leftChild=newNode;
+                    break;
+                }
+                //if current is not null (there is a node in the left) jump to that node
+                current=current.leftChild;
+            }
+            else {
+                if (current.rightChild==null){
+                    //if right is null insert there
+                    current.rightChild=newNode;
+                    break;
+                }
+                current=current.rightChild;
+            }
+        }
+    }
 }
