6a7245092bfb322b406906c8a940a33fe7c73309
nihano <nioztekin@gmail.com>
2023-07-19 00:18:52 +0300

    Tree app

--- src/main/java/self_practice/trees/TreeApp.java
+++ src/main/java/self_practice/trees/TreeApp.java
@@ -17,5 +17,20 @@ public class TreeApp {
         tree.postOrderTraversal(tree.root);
         System.out.println("\nLevel Order Traversal");
         tree.levelOrderTraversal();
+
+        MyTree tree2 = new MyTree();
+        int[] numbers2 = new int[]{10, 6, 8, 20, 4, 17, 42};
+        for (int i = 0; i <7 ; i++) {
+            tree2.insert(numbers2[i]);
+        }
+        System.out.println();
+        VisualizeTree.printTree(tree2.root, null, false);
+        System.out.println(tree2.numberOfNodesFBT(tree2.root));
+
+        MyTree emptyTree = new MyTree();
+
+
+//        System.out.println(emptyTree.contains(1));
     }
+
 }
