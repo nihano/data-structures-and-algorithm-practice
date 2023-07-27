377766a6b6e9ba13c87af2d3c9e4234d53c2392a
nihano <nioztekin@gmail.com>
2023-07-28 00:06:52 +0300

    Linked List Palindrome clean code

---
+++ src/main/java/leet_code/_234_PalindromeLinkedList/SolutionCleanCode.java
@@ -0,0 +1,45 @@
+package leet_code._234_PalindromeLinkedList;
+
+public class SolutionCleanCode {
+
+    public static Node reverse(Node slow) {
+        Node prev = null;
+        Node curr = slow;
+        while (curr != null) {
+            Node nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome2(Node head) {
+        Node prev = head;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        prev.next = null;//break the link
+        if (fast != null) {
+
+            Node temp = slow;
+            slow = slow.next;
+            temp.next = null;
+        }
+
+        Node p1 = head;
+        Node p2 = reverse(slow);
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return true;
+    }
+}
