fbc3b78fe445ad4a9f75271c6f0ccc6c03e48d29
nihano <nioztekin@gmail.com>
2023-07-28 00:06:26 +0300

    Linked List Palindrome

---
+++ src/main/java/leet_code/_234_PalindromeLinkedList/Test.java
@@ -0,0 +1,17 @@
+package leet_code._234_PalindromeLinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+        MySLL mySLL = new MySLL();
+        mySLL.add(1);
+        mySLL.add(2);
+        mySLL.add(2);
+        mySLL.add(1);
+        mySLL.add(1);
+
+//        System.out.println(Solution.isPalindrome(mySLL.head));
+        System.out.println(SolutionCleanCode.isPalindrome2(mySLL.head));
+
+
+    }
+}
