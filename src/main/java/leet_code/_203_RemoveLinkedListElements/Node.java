d64a944c68a4767f17cdf66e415db308f8b6b02e
nihano <nioztekin@gmail.com>
2023-07-29 16:54:54 +0300

    Remove linked list elements

---
+++ src/main/java/leet_code/_203_RemoveLinkedListElements/Node.java
@@ -0,0 +1,14 @@
+package leet_code._203_RemoveLinkedListElements;
+
+import lombok.Getter;
+
+@Getter
+public class Node {
+
+    int val;
+    Node next;
+
+    public Node(int id) {
+        this.val = id;
+    }
+}
