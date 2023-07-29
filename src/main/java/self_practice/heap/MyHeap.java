2cf411c401b590d1ef951835a7e74d69caaf620c
nihano <nioztekin@gmail.com>
2023-07-29 19:55:34 +0300

    remove added

--- src/main/java/self_practice/heap/MyHeap.java
+++ src/main/java/self_practice/heap/MyHeap.java
@@ -9,37 +9,38 @@ public class MyHeap {
 
     public MyHeap(int capacity) {
         this.items = new int[capacity];
-        this.size=0;
+        this.size = 0;
     }
-    public int peek(){
-        if (size==0) throw new NoSuchElementException();
+
+    public int peek() {
+        if (size == 0) throw new NoSuchElementException();
         return items[0];//return the root value
     }
 
-    public void insert(int value){
-        if (size==items.length) throw new IllegalStateException();
+    public void insert(int value) {
+        if (size == items.length) throw new IllegalStateException();
         else {
-            items[size++]=value;
+            items[size++] = value;
             bubbleUp();
         }
     }
 
-    public void bubbleUp(){
+    public void bubbleUp() {
         //calculate start point/index
-        int index=size-1;
+        int index = size - 1;
         //while index >0 && parent is less than child contunue...
         //1. swap child and parent
         //calculate next index=parent index
-        while (index>0 && items[index]>items[parentIndex(index)]){
+        while (index > 0 && items[index] > items[parentIndex(index)]) {
             //swap
             swap(index, parentIndex(index));
             //assign index with the parent index
-            index=parentIndex(index);
+            index = parentIndex(index);
         }
     }
 
-    public int parentIndex(int index){
-        return (index-1)/2;
+    public int parentIndex(int index) {
+        return (index - 1) / 2;
     }
 
     public void swap(int first, int second) {
@@ -47,14 +48,70 @@ public class MyHeap {
         items[first] = items[second];
         items[second] = temp;
     }
-    public void printHeap(){
+
+    public void printHeap() {
         for (int i = 0; i < size; i++) {
-            System.out.print(items[i]+", ");
+            System.out.print(items[i] + ", ");
         }
         System.out.println();
     }
 
-    public int length(){
+    public int remove() {
+        if (size == 0) throw new NoSuchElementException();
+        else {
+            int result = items[0];
+            items[0] = items[--size];
+            bubbleDown();
+            return result;
+        }
+    }
+
+    public void bubbleDown(){
+        int index=0;
+        int largerChildIndex;
+        while (index<=size && !isValidParent(index)){
+            largerChildIndex=largerChildIndex(index);
+            swap(index,largerChildIndex);
+            index=largerChildIndex;
+        }
+    }
+
+    public boolean isValidParent(int index){
+        if (!hasLeftChild(index)) return true;
+        else{
+            boolean isValid=false;
+            if (hasRightChild(index)){
+                isValid= (items[index]>=items[leftChildIndex(index)] && items[index]>=items[rightChildIndex(index)]);
+            }
+            return isValid;
+        }
+    }
+
+    public boolean hasLeftChild(int index){
+        return leftChildIndex(index)<=size;
+    }
+
+    public boolean hasRightChild(int index){
+        return rightChildIndex(index)<=size;
+    }
+
+    public int leftChildIndex(int index){
+        return index*2+1;
+    }
+
+    public int rightChildIndex(int index){
+        return index*2+2;
+    }
+
+    public int largerChildIndex(int index){
+        if (!hasLeftChild(index)) return index;
+        else if (!hasRightChild(index)) {
+            return leftChildIndex(index);
+        }
+        return items[leftChildIndex(index)]>items[rightChildIndex(index)] ? leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    public int length() {
         return items.length;
     }
 
