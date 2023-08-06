2b00493889f153b667059c235506248fdd99a057
nihano <nioztekin@gmail.com>
2023-08-06 18:00:20 +0300

    review iterative depth first search

---
+++ src/main/java/self_practice/trees/review/TreeApp.java
@@ -0,0 +1,20 @@
+package self_practice.trees.review;
+
+public class TreeApp {
+    public static void main(String[] args) {
+        TreeClass tree=new TreeClass();
+        System.out.println(tree.root);
+        tree.insert(new TreeNode("John", "locke",5));
+        tree.insert(new TreeNode("Carol", "smith",3));
+        tree.insert(new TreeNode("Betul", "QQ",6));
+        tree.insert(new TreeNode("Kadifa", "WW",4));
+        tree.preOrderTraversal(tree.root);
+        System.out.println();
+        tree.preOrderTraversalIterative(tree.root);
+        //tree.levelOrderTraversal();
+        System.out.println();
+       // tree.inOrderTraversalIterative(tree.root);
+        System.out.println();
+        tree.inOrderTraversal(tree.root);
+    }
+}
