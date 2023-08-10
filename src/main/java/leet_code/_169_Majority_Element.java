7cefcd2dc57f5fd785e875f7c875942e05ec73a6
nihano <nioztekin@gmail.com>
2023-08-11 00:47:15 +0300

    solution

---
+++ src/main/java/leet_code/_169_Majority_Element.java
@@ -0,0 +1,22 @@
+package leet_code;
+
+import java.util.Arrays;
+
+public class _169_Majority_Element {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 1, 1, 1, 2, 1};
+        //-1,1,1,1,1,2
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
+
+
