7c70a19cd6bbe0846cf2a809b236dc4da296eb03
nihano <nioztekin@gmail.com>
2023-08-11 02:04:36 +0300

    solution

---
+++ src/main/java/leet_code/_125_ValidPalindrome.java
@@ -0,0 +1,33 @@
+package leet_code;
+
+public class _125_ValidPalindrome {
+    public static void main(String[] args) {
+        String s= "race a car";
+        System.out.println(isPalindrome(s));
+
+    }
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst )) {
+                start++;
+            } else if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+
+}
