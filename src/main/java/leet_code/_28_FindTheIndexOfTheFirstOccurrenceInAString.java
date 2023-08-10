cd7c883c2cbcc3690d0b01f4b21c49d16dfe9a2a
nihano <nioztekin@gmail.com>
2023-08-11 01:07:48 +0300

    solution

---
+++ src/main/java/leet_code/_28_FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,22 @@
+package leet_code;
+
+public class _28_FindTheIndexOfTheFirstOccurrenceInAString {
+    public static void main(String[] args) {
+        String s= "aadasd";
+        System.out.println(s.substring(0, 6));
+        System.out.println(strStr("codeleet", "leet"));
+    }
+    public static int strStr(String haystack, String needle){
+        int begin=0;
+        int end=needle.length();
+        while (end<=haystack.length()){
+            if (haystack.substring(begin,end).equals(needle)){
+                return begin;
+            }else {
+                begin++;
+                end++;
+            }
+        }
+        return -1;
+    }
+}
