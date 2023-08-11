41c6d6694b0642dfbfda035e004430d6569280b1
nihano <nioztekin@gmail.com>
2023-08-11 16:04:29 +0300

    solution

---
+++ src/main/java/leet_code/_27_Remove_Element.java
@@ -0,0 +1,21 @@
+package leet_code;
+
+import java.util.Arrays;
+
+public class _27_Remove_Element {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(removeElement(nums, 3));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
