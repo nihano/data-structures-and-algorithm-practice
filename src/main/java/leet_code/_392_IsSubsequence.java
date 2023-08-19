1013cbfb5c18b4829354657dd34ff4659502d239
nihano <nioztekin@gmail.com>
2023-08-19 03:40:50 +0300

    solution

---
+++ src/main/java/leet_code/_392_IsSubsequence.java
@@ -0,0 +1,22 @@
+package leet_code;
+
+class _392_IsSubsequence {
+    public static void main(String[] args) {
+
+        String s=""; //0 1 2
+        String t="ahbgdc"; //0 3 5
+        System.out.println(isSubsequence(s, t));
+    }
+    public static boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while (i<s.length() && j<t.length()){
+            if (s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+            return i==s.length();
+    }
+
+}
