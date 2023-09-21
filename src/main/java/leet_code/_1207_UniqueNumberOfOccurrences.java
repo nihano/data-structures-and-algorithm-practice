2cd315aac3062121b0f0cb36becbdea47c0880b3
nihano <nioztekin@gmail.com>
2023-09-21 22:56:20 +0300

    solution

---
+++ src/main/java/leet_code/_1207_UniqueNumberOfOccurrences.java
@@ -0,0 +1,34 @@
+package leet_code;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class _1207_UniqueNumberOfOccurrences {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1};
+        System.out.println(uniqueOccurrences(arr));
+    }
+
+//Unique number of occurrences
+    public static boolean uniqueOccurrences(int[] arr){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int each : arr) {
+            if (!map.containsKey(each)){
+                map.put(each, 1);
+            }else {
+                map.put(each, map.get(each)+1);
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (Integer value : map.values()) {
+            if (!set.add(value)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
