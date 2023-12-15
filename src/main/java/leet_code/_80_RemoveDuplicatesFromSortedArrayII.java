237773c12789891734d98f8d5533363aa7f5b77c
nihano <nioztekin@gmail.com>
2023-12-15 20:38:25 +0300

    solution

---
+++ src/main/java/leet_code/_80_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,29 @@
+package leet_code;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class _80_RemoveDuplicatesFromSortedArrayII {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+    }
+    public  static int removeDuplicates(int[] nums) {
+        int l = 0, r = 0;
+        while (r < nums.length) {
+            int count = 1;
+            while (r + 1 < nums.length && nums[r]==nums[r+1]) {
+                r++;
+                count++;
+            }
+            for (int i = 0; i < Math.min(2, count); i++) {
+                nums[l]=nums[r];
+                l++;
+            }
+            r++;
+        }
+
+        System.out.println(Arrays.toString(nums));
+        return l;
+    }
+}
