0d713baafb54b60fa84d0bf4280c0d9de079fae0
nihano <nioztekin@gmail.com>
2023-12-15 20:14:49 +0300

    solution

---
+++ src/main/java/leet_code/_80_RemoveDuplicatesFromSortedArrayII_AnotherSolution.java
@@ -0,0 +1,17 @@
+package leet_code;
+
+public class _80_RemoveDuplicatesFromSortedArrayII_AnotherSolution {
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i - 2])
+                nums[i++] = n;
+        return i;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        System.out.println(removeDuplicates(nums));
+    }
+}
