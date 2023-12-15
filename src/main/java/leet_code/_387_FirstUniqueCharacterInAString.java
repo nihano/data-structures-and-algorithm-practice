831f932db967d4c84caffffca51277851f121409
nihano <nioztekin@gmail.com>
2023-12-15 20:38:03 +0300

    solution

---
+++ src/main/java/leet_code/_387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,37 @@
+package leet_code;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class _387_FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+
+
+    }
+
+
+
+    public static int firstUniqChar(String s) {
+        Set<Character> seen= new HashSet<>();
+        Set<Character> repeating= new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!seen.contains(s.charAt(i))){
+                seen.add(s.charAt(i));
+            }else{
+                repeating.add(s.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (repeating.add(s.charAt(i))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
