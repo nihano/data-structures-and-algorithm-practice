f02d8845bb896056f6a9324876fdcb41578ca724
nihano <nioztekin@gmail.com>
2023-12-16 14:55:19 +0300

    solution

--- src/main/java/leet_code/_1343_NumberOfSubarraysSizeKThreshold.java
+++ src/main/java/leet_code/_1343_NumberOfSubarraysSizeKThreshold.java
@@ -1,23 +1,28 @@
 package leet_code;
 
-class _1343_NumberOfSubarraysSizeKThreshold {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
+public class _1343_NumberOfSubarraysSizeKThreshold {
+    public static int numOfSubarrays(int[] arr, int k, int threshold) {
 
-        int currentSum=0;
-        int count=0;
+        int currentSum = 0;
+        int count = 0;
 
-        for (int i=0; i<arr.length; i++){
-            currentSum+=arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
 
-            if(i>=k-1){
-                if((currentSum/k)>=threshold){
+            if (i >= k - 1) {
+                if ((currentSum / k) >= threshold) {
                     count++;
                 }
-                 currentSum-=arr[i-(k-1)];
+                currentSum -= arr[i - (k - 1)];
             }
         }
 
         return count;
-        
+
     }
+
+    public static void main(String[] args) {
+        System.out.println(numOfSubarrays(new int[]{2, 2, 2, 2, 5, 5, 5, 8}, 3, 4));
+    }
+
 }
