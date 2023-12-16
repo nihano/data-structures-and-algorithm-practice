323223c7cee0ef9da276f650b3465c1074cf0f2e
nihano <nioztekin@gmail.com>
2023-12-16 21:54:43 +0300

    solution

---
+++ src/main/java/leet_code/_1328_BreakAPalindrome.java
@@ -0,0 +1,29 @@
+package leet_code;
+
+public class _1328_BreakAPalindrome {
+
+    public static String breakPalindrome(String palindrome) {
+        int length = palindrome.length();
+        //aaa.  abcd
+        if (length == 1) return "";
+        char[] chPalindrome = palindrome.toCharArray();
+        for (int i = 0; i < length / 2; i++) {
+            if (chPalindrome[i] != 'a') {
+                chPalindrome[i] = 'a';
+                return String.valueOf(chPalindrome);
+            }
+        }
+
+        chPalindrome[length - 1] = 'b';
+        return String.valueOf(chPalindrome);
+
+    }
+
+    public static void main(String[] args) {
+        String palindrome = "racecar";
+        System.out.println(breakPalindrome(palindrome));
+    }
+}
+
+
+
