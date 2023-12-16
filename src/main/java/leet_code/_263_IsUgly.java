d69642a60a498f24efbb7c107f157ebe96a6175e
nihano <nioztekin@gmail.com>
2023-12-16 14:48:17 +0300

    solution

---
+++ src/main/java/leet_code/_263_IsUgly.java
@@ -0,0 +1,27 @@
+package leet_code;
+
+public class _263_IsUgly {
+
+    public static boolean isUgly(int n) {
+        if (n < 1) return false;
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+        return n == 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(isUgly(0));
+
+    }
+}
