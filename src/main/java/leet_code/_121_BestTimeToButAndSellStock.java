9c44b6ff79d04653e2b010228d64dff2999a1f8c
nihano <nioztekin@gmail.com>
2024-02-04 22:54:43 +0300

    solution

---
+++ src/main/java/leet_code/_121_BestTimeToButAndSellStock.java
@@ -0,0 +1,25 @@
+package leet_code;
+
+public class _121_BestTimeToButAndSellStock {
+    public int maxProfit(int[] prices) {
+
+        int l=0;
+        int r=1;
+        int max=0;
+        while(r<prices.length){
+            //calculate profit each time and update max acc.
+            if(prices[l]<prices[r]){
+                max=Math.max(max, (prices[r]-prices[l]));
+            }else{
+                //if right is smaller move l all the way to the right
+                l=r;
+            }
+            //move right
+            r++;
+        }
+
+        return max;
+
+    }
+}
+
