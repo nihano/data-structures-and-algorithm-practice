00a18e1dc1486092810a0f47d16f49cfaaad5863
nihano <nioztekin@gmail.com>
2024-02-05 20:43:58 +0300

    solution

---
+++ src/main/java/leet_code/_500_KeyboardRow.java
@@ -0,0 +1,53 @@
+package leet_code;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class _500_KeyboardRow {
+    public static String[] findWords(String[] words) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (isOneRow(word)) {
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+
+    private static int check(char ch) {
+        ch = Character.toLowerCase(ch);
+        if ("qwertyuiop".indexOf(ch) != -1) {
+            return 1;
+        } else if ("asdfghjkl".indexOf(ch) != -1) {
+            return 2;
+        } else if ("zxcvbnm".indexOf(ch) != -1) {
+            return 3;
+        }
+        return 0;
+    }
+
+    private static boolean isOneRow(String s) {
+        if (s.length() == 1) {
+            return true;
+        }
+        Set<Integer> rowSet = new HashSet<>();
+        rowSet.add(check(s.charAt(0)));
+        for (int i = 1; i < s.length(); i++) {
+            if (!rowSet.contains(check(s.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"Hello","Alaska","Dad","Peace"};
+        String[] words2 = new String[]{"adsdf","sfd"};
+        String[] words3 = new String[]{"omk"};
+        System.out.println(Arrays.toString(findWords(words)));
+        System.out.println(Arrays.toString(findWords(words2)));
+        System.out.println(Arrays.toString(findWords(words3)));
+
+    }
+
+}
