058daeb297e567915afac43be8c4f9b6b2d5a638
nihano <nioztekin@gmail.com>
2024-02-08 00:31:02 +0300

    solution

---
+++ src/main/java/leet_code/_1816_TruncateString.java
@@ -0,0 +1,22 @@
+package leet_code;
+
+class _1816_TruncateString {
+    public String truncateSentence(String s, int k) {
+       int count=0;
+       int i=0;
+       while(count<k && i<s.length()){
+           if(s.charAt(i)==' '){
+               count++;
+               if(k==count){
+                   break;
+               }
+           }
+
+           i++;
+       }
+
+       return s.substring(0, i);
+
+        
+    }
+}
