c956da7aa8e11c8f6457cd2d0246b13469c9c0ea
nihano <nioztekin@gmail.com>
2024-02-08 00:30:53 +0300

    solution

---
+++ src/main/java/leet_code/_66_PlusOne.java
@@ -0,0 +1,27 @@
+package leet_code;
+
+import java.util.Arrays;
+
+class _66_PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int j = digits.length;
+        for(int i=j-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }else{
+                digits[i]=0;
+            }
+        }
+
+        int[] arr = new int[j+1];
+        arr[0]=1;
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1,2,3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+}
