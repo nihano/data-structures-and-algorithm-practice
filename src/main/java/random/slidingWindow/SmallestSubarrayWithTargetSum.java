706b2672c6df617872fa3dc9ef42ae56024974a3
nihano <nioztekin@gmail.com>
2024-02-11 00:44:19 +0300

    Solution

--- src/main/java/random/slidingWindow/SmallestSubarrayWithTargetSum.java
+++ src/main/java/random/slidingWindow/SmallestSubarrayWithTargetSum.java
@@ -3,21 +3,21 @@ package random.slidingWindow;
 public class SmallestSubarrayWithTargetSum {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{4, 2, 2, 7, 8, 1, 2, 8, 10};
-        System.out.println(smallestSubarray(arr, 8));
+        int[] nums = new int[]{4, 2, 2, 7, 8, 1, 2, 8, 10};
+        System.out.println(smallestSubarray(nums, 8));
 
     }
 
-    public static int smallestSubarray(int[] arr, int target) {
+    public static int smallestSubarray(int[] nums, int target) {
         int currentSum = 0;
         int smallestSize = Integer.MAX_VALUE;
         int indexBeg = 0;
 
-        for (int indexEnd = 0; indexEnd < arr.length; indexEnd++) {
-            currentSum += arr[indexEnd];
+        for (int indexEnd = 0; indexEnd < nums.length; indexEnd++) {
+            currentSum += nums[indexEnd];
             while (currentSum >= target) {
                 smallestSize=Math.min(smallestSize, indexEnd-indexBeg+1);
-                currentSum-=arr[indexBeg];
+                currentSum-=nums[indexBeg];
                 indexBeg++;
             }
         }
