9adcb3ee4fb864d1e21aea17df298b77d1474edf
nihano <nioztekin@gmail.com>
2024-02-11 00:42:54 +0300

    Solution

---
+++ src/main/java/random/question/IAnimal.java
@@ -0,0 +1,19 @@
+package random.question;
+
+public interface IAnimal {
+    void setId(int id);
+
+    int getId();
+
+    void setSpecies(String species);
+
+    String getSpecies();
+
+    void setName(String name);
+
+    String getName();
+
+    void setAge(int age);
+
+    int getAge();
+}
