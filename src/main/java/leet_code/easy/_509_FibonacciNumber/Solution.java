8fc207150c149af918ca9fa23fe96f31f659d60d
nihano <nioztekin@gmail.com>
2024-02-21 22:57:35 +0300

    Solution

---
+++ src/main/java/leet_code/easy/_509_FibonacciNumber/Solution.java
@@ -0,0 +1,17 @@
+package leet_code.easy._509_FibonacciNumber;
+
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int prev=0;
+        int next=1;
+        int curr=0;
+        for (int i=2; i<=n; i++){
+            curr= prev+next;
+            prev=next;
+            next=curr;
+        }
+
+        return next;
+    }
+}
